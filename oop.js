9594d0a0b7404600c86a6b3a30253a2e41eb1347
oreva <orevaawusi@gmail.com>
2024-08-29 13:47:36 +0100

    oop added

---
+++ oop.js
@@ -0,0 +1,13 @@
+class User{
+    constructor(name, email, phone){
+        this.name = name
+        this.email = email
+        this.phone = phone
+    }
+
+    
+}
+
+let newUser = new User("oreva", "@gmail.com", 123)
+
+console.log(newUser)
